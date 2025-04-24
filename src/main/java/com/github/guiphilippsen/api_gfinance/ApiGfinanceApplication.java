e53a44d7a79a141cb4b712acc13b2b01a07fab2e
guilhermephilippsen2003@gmail.com <guilhermephilippsen2003@gmail.com>
2025-04-24 13:07:59 -0300

    first commit

---
+++ src/main/java/com/github/guiphilippsen/api_gfinance/ApiGfinanceApplication.java
@@ -0,0 +1,13 @@
+package com.github.guiphilippsen.api_gfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGfinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGfinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/guiphilippsen/api_gfinance/ApiGfinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.guiphilippsen.api_gfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGfinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
